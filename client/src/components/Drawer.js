044730626811461753293ddf8a7be564c31ed7e7
Lilit <lilit.asatryan.ec@gmail.com>
2023-08-25 01:19:15 +0400

    Drawer and Admin desktop changes

--- client/src/components/Drawer.js
+++ client/src/components/Drawer.js
@@ -1,22 +1,23 @@
+//Drawer.js
 import * as React from 'react';
-import { Drawer, ListItem, ListItemText } from '@mui/material';
-//drawer.js
+import { Drawer, ListItem, ListItemText, Button } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-
+import { useState } from 'react';
+const drawer_data = [
+  { name: "About Us"},
+];
 export const MyBar = () => {
     const theme = createTheme();
-
-    const drawer_data = [
-        { name: "About Us"},
-    ];
+ 
+    const [open, setOpen] = useState(false);
 
     const getList = () => (
-        <div sx={{display: 'flex', justifyContent: 'center', mt: 50}}>
+        <div sx={{display: 'flex', justifyContent: 'center', mt: 50}} onClick={()=>setOpen(false)}>
           {drawer_data.map((item, index) => (
             <ListItem key={index} >
               <ListItemText primary={item.name} sx={{ textAlign: 'center', mt: 50}} primaryTypographyProps={{
                   style: {
-                    fontSize: '18px'
+                    fontSize: '30px'
                   }
                 }}
               />
@@ -30,7 +31,7 @@ export const MyBar = () => {
             <Drawer 
                 variant='permanent' 
                 anchor={"left"} 
-                PaperProps={{sx: {backgroundColor: "#1f4700", color: "white", width:300}}}>
+                PaperProps={{sx: {backgroundColor: "#1f4700", color: "white", width: "300px" }}}>
                 {getList()}
             </Drawer>
         </ThemeProvider>
