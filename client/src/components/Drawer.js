eb57f51a7963a324012737009bbeb58a88836e1c
Lilit <lilit.asatryan.ec@gmail.com>
2023-08-28 03:10:43 +0400

    Changes

--- client/src/components/Drawer.js
+++ client/src/components/Drawer.js
@@ -1,37 +1,70 @@
-//Drawer.js
+// Drawer.js
 import * as React from 'react';
-import { Drawer, ListItem, ListItemText, Button } from '@mui/material';
+import { Box, Drawer, Button } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { useState } from 'react';
+
 const drawer_data = [
-  { name: "About Us"},
+  { 
+    name: "About Us", 
+    link: "#/dashboard"
+  },
+  {
+    name: "Info Page", 
+    link: "#/adminPage"
+  },
+  {
+    name: "Settings",
+    link: "#/settings"
+  }
 ];
+
 export const MyBar = () => {
     const theme = createTheme();
  
     const [open, setOpen] = useState(false);
 
     const getList = () => (
-        <div sx={{display: 'flex', justifyContent: 'center', mt: 50}} onClick={()=>setOpen(false)}>
+        <Box 
+          sx={{
+            display: 'flex',
+            flexDirection: 'column', // Set the flex direction to 'column'
+            justifyContent: 'center', 
+            mt: 15
+          }} 
+          onClick={()=>setOpen(false)}
+        >
           {drawer_data.map((item, index) => (
-            <ListItem key={index} >
-              <ListItemText primary={item.name} sx={{ textAlign: 'center', mt: 50}} primaryTypographyProps={{
-                  style: {
-                    fontSize: '30px'
-                  }
-                }}
-              />
-            </ListItem>
+            <Button 
+              key={index} 
+              component="a" 
+              href={item.link} 
+              variant="contained" // Add variant="contained" for button styling
+              color="primary" // Add color="primary" for primary button color
+              sx={{
+                background: "#1f4700",
+                width: "100%",
+                mt: 5,
+                fontFamily: "Roboto", 
+                color: 'white', 
+                '&:hover': {
+                  background: 'rgba(0,190,0,1)',
+                  textDecoration: 'none', 
+                },
+              }}
+            >
+              {item.name}
+            </Button>
           ))}
-        </div>
-      );
+        </Box>
+    );
 
     return (
         <ThemeProvider theme={theme}>
             <Drawer 
                 variant='permanent' 
                 anchor={"left"} 
-                PaperProps={{sx: {backgroundColor: "#1f4700", color: "white", width: "300px" }}}>
+                PaperProps={{sx: {backgroundColor: "#1f4700", color: "white", width: "20%" }}}>
                 {getList()}
             </Drawer>
         </ThemeProvider>
