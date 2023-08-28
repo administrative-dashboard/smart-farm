02aaed5f201809a3058353423b0ff0fea1c86280
Ani Hambaryan <ani.hambaryan.ec@gmail.com>
2023-08-28 21:08:54 +0400

    server migration

---
+++ server/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.PG_HOST,
+      port: Number(process.env.PG_PORT),
+      username: process.env.PG_USERNAME,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DATABASE,
+      models: [],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
---
+++ server/src/database/migrations/20230828143437-users.js
@@ -0,0 +1,39 @@
+// require('dotenv').config();
+
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const tableName = 'users';
+    const columns = {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      phone_number: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      profile_image: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+    };
+
+    await queryInterface.createTable(tableName, columns);
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('users');
+  }
+};
