280963348be0f522dcedf3dd77b6e30d58afe841
admininstrative-dashboard <140819251+admininstrative-dashboard@users.noreply.github.com>
2023-09-12 14:06:21 +0400

    Update 20230829122751-demo_permissions.js

--- server/src/database/seeders/20230829122751-demo_permissions.js
+++ server/src/database/seeders/20230829122751-demo_permissions.js
@@ -4,7 +4,25 @@
 module.exports = {
   async up(queryInterface) {
     await queryInterface.bulkInsert('permissions', [
-      { value: '' }
+        { value: 'VIEW_MAIN_DASHBOARD' },
+        { value: 'VIEW_PROFILE_INFO' },
+        { value: 'EDIT_PROFILE_INFO' },
+        { value: 'ADD_GREENHOUSE' },
+        { value: 'DELETE_GREENHOUSE' },
+        { value: 'CREATE_FIELD' },
+        { value: 'DELETE_FIELD' },
+        { value: 'CREATE_FIXED_DEVICE' },
+        { value: 'DELETE_FIXED_DEVICE' },
+        { value: 'CREATE_PORTABLE_DEVICE' },
+        { value: 'DELETE_PORTABLE_DEVICE' },
+        { value: 'VIEW_OWN_INFO' },
+        { value: 'VIEW_PLANNING_INFO' },
+        { value: 'EDIT_PLANNING' },
+        { value: 'VIEW_COMMUNITY_INFO' },
+        { value: 'ADD_ROLE' },
+        { value: 'ADD_PERMISSION' },
+        { value: 'CREATE_ROLE' },
+        { value: 'VIEW_ALL_INFO' },
     ], {});
   },
 
