ea22bca984ed45a6f6fbd96c3b69a3eee13d941b
Your Name <you@example.com>
2023-09-25 16:17:29 +0400

    edit role

--- client/src/providers/dataProvider.js
+++ client/src/providers/dataProvider.js
@@ -1,8 +1,6 @@
-import simpleRestProvider from "ra-data-simple-rest";
-import { getJwtTokenFromCookies } from "./authUtils";
-import { API_URL } from "../consts";
-
-const apiUrl = API_URL;
+import simpleRestProvider from 'ra-data-simple-rest';
+import { getJwtTokenFromCookies } from './authUtils'; 
+const apiUrl = 'http://localhost:5000'; 
 const dataProvider = simpleRestProvider(apiUrl);
 const customDataProvider = {
   ...dataProvider,
@@ -14,50 +12,43 @@ const customDataProvider = {
     const { filter, pagination, sort } = params;
     const query = {};
 
-    for (const prop in filter) {
-      if (filter[prop]) 
+    for(const prop in filter){
+      if(filter[prop])
       query[prop] = filter[prop];
     }
-    for (const prop in pagination) {
-      query[prop] = pagination[prop];
-    }
-    for (const prop in sort) {
-      query[prop] = sort[prop];
-    }
     console.log("query: ", query);
+
     try {
-      const response = await fetch(
-        `${apiUrl}/${resource}?${new URLSearchParams(query)}`,
-        {
-          method: "GET",
-          headers,
-        }
-      );
+
+      const response = await fetch(`${apiUrl}/${resource}?${new URLSearchParams(query)}`, {
+        method: 'GET',
+        headers,
+      });
 
       if (!response.ok) {
         throw new Error(response.statusText);
       }
 
-      const { data, total } = await response.json();
+      const data = await response.json();
       return {
         data: data,
-        total: total,
+        total: data.length,
       };
     } catch (error) {
       throw new Error(`Error fetching ${resource}: ${error.message}`);
     }
   },
   async create(resource, params) {
-    const token = getJwtTokenFromCookies();
+    const token = getJwtTokenFromCookies(); 
     const headers = new Headers({
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`, 
+      'Content-Type': 'application/json', 
     });
     try {
       const response = await fetch(`${apiUrl}/${resource}`, {
         method: "POST",
         headers,
-        body: JSON.stringify(params.data),
+        body: JSON.stringify(params.data), 
       });
       if (!response.ok) {
         throw new Error(response.statusText);
@@ -70,32 +61,32 @@ const customDataProvider = {
       throw new Error(`Error creating ${resource}: ${error.message}`);
     }
   },
-
+  
   async getOne(resource, params) {
-    const token = getJwtTokenFromCookies();
+    const token = getJwtTokenFromCookies(); 
     const headers = new Headers({
-      Authorization: `Bearer ${token}`,
+      Authorization: `Bearer ${token}`, 
     });
-
+  
     if (!params.id) {
       throw new Error('Not set parameter "id"');
     }
-
+  
     try {
       const response = await fetch(`${apiUrl}/${resource}/${params.id}`, {
-        method: "GET",
+        method: 'GET',
         headers,
       });
-
+  
       if (!response.ok) {
         throw new Error(response.statusText);
       }
-
+  
       const data = await response.json();
       if (!data.id) {
         throw new Error('API response is missing the "id" attribute');
       }
-
+  
       return {
         data: data,
       };
@@ -103,18 +94,21 @@ const customDataProvider = {
       throw new Error(`Request Error ${resource}: ${error.message}`);
     }
   },
+  
+  
+ 
 
   async update(resource, params) {
-    const token = getJwtTokenFromCookies();
+    const token = getJwtTokenFromCookies(); 
     const headers = new Headers({
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`, 
+      'Content-Type': 'application/json', 
     });
     try {
       const response = await fetch(`${apiUrl}/${resource}/${params.id}`, {
         method: "PUT",
         headers,
-        body: JSON.stringify(params.data),
+        body: JSON.stringify(params.data), 
       });
       if (!response.ok) {
         throw new Error(response.statusText);
@@ -127,33 +121,35 @@ const customDataProvider = {
       throw new Error(`Error updating ${resource}: ${error.message}`);
     }
   },
+  
 
   async delete(resource, params) {
-    const token = getJwtTokenFromCookies();
+    const token = getJwtTokenFromCookies(); 
     const headers = new Headers({
-      Authorization: `Bearer ${token}`,
+      Authorization: `Bearer ${token}`, 
     });
-
+  
     if (!params.id) {
       throw new Error('Not set parameter "id"');
     }
-
+  
     try {
       const response = await fetch(`${apiUrl}/${resource}/${params.id}`, {
-        method: "DELETE",
+        method: 'DELETE',
         headers,
       });
-
+  
       if (!response.ok) {
         throw new Error(response.statusText);
       }
-
+  
       return {
-        data: params.id,
+        data: params.id, 
       };
     } catch (error) {
       throw new Error(`Error deleting from ${resource}: ${error.message}`);
     }
-  },
+  }
+  
 };
 export default customDataProvider;
