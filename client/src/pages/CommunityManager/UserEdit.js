a8090beaa63e5fc0050370462eaae0e20f25a0c7
Ani <ani.hambaryan.ec@gmail.com>
2023-09-28 00:17:10 +0400

    manager

--- client/src/pages/CommunityManager/UserEdit.js
+++ client/src/pages/CommunityManager/UserEdit.js
@@ -1,4 +1,4 @@
-// //client//pages/owner/UserEd.js
+
 // import React, { useEffect, useState } from "react";
 // import {
 //   Edit,
@@ -6,6 +6,7 @@
 //   TextInput,
 //   DateInput,
 //   NumberInput,
+//   SelectInput, // Import SelectInput
 //   useDataProvider,
 // } from "react-admin";
 // import { Box, MenuItem, TextField } from "@mui/material";
@@ -34,18 +35,21 @@
 //       });
 //   }, []);
 
-
 //   return (
 //     <>
-//       <Edit 
-//         title="Edit a user" {...props}
-//         resource="community/users"
-//       >
+//       <Edit title="Edit a user" {...props} resource="community/users">
 //         <SimpleForm>
 //           {/* <NumberInput source="id" disable /> */}
 //           <TextInput source="name" label="Name" />
 //           <TextInput source="phone_number" label="Phone_number" />
-//           <TextInput source="roles" label="Role" />
+//           <SelectInput
+//             source="roles"
+//             label="Role"
+//             choices={roles.map((role) => ({
+//               id: role.value,
+//               name: role.value,
+//             }))}
+//           />
 //         </SimpleForm>
 //       </Edit>
 //       <Box
@@ -53,34 +57,28 @@
 //         flexDirection="row"
 //         justifyContent="center"
 //         alignItems="center"
-//       >
-
-//       </Box>
+//       ></Box>
 //     </>
 //   );
 // };
 
+// UserEdit.js
 import React, { useEffect, useState } from "react";
 import {
   Edit,
   SimpleForm,
   TextInput,
-  DateInput,
-  NumberInput,
-  SelectInput, // Import SelectInput
-  useDataProvider,
+  SelectArrayInput, // Use SelectArrayInput for multi-select
 } from "react-admin";
-import { Box, MenuItem, TextField } from "@mui/material";
-
-import { HomeRedirectButton } from "../../components/HomeRedirectButton";
-import customDataProvider from "../../providers/dataProvider";
 import axios from "axios";
 import { API_URL } from "../../consts";
 import { getJwtTokenFromCookies } from "../../providers/authUtils";
+import { Box } from "@mui/material";
 
 export const UserEdit = (props) => {
-  const [selectedRole, setSelectedRole] = useState("");
   const [roles, setRoles] = useState([]);
+  const [selectedRoles, setSelectedRoles] = useState([]);
+
   useEffect(() => {
     axios
       .get(`${API_URL}/user/roles`, {
@@ -94,31 +92,38 @@ export const UserEdit = (props) => {
       .catch((error) => {
         console.error("Error fetching roles: ", error);
       });
-  }, []);
+
+    // Ensure props.record exists and contains 'roles' property
+    if (props.record && props.record.roles) {
+      setSelectedRoles(props.record.roles);
+    }
+  }, [props.record]);
 
   return (
     <>
-      <Edit title="Edit a user" {...props} resource="community/users">
-        <SimpleForm>
-          {/* <NumberInput source="id" disable /> */}
-          <TextInput source="name" label="Name" />
-          <TextInput source="phone_number" label="Phone_number" />
-          <SelectInput
-            source="roles"
-            label="Role"
-            choices={roles.map((role) => ({
-              id: role.value,
-              name: role.value,
-            }))}
-          />
-        </SimpleForm>
-      </Edit>
-      <Box
-        display="flex"
-        flexDirection="row"
-        justifyContent="center"
-        alignItems="center"
-      ></Box>
-    </>
+    <Edit title="Edit a user" {...props} resource="community/users">
+      <SimpleForm>
+        <TextInput source="name" label="Name" />
+        <TextInput source="phone_number" label="Phone_number" />
+        <SelectArrayInput
+  source="roles"
+  label="Roles"
+  choices={roles.map((role) => ({
+    id: role.value,
+    name: role.value,
+  }))}
+  initialValue={selectedRoles || []} // Ensure it's an array
+/>
+      </SimpleForm>
+    </Edit>
+    <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="center"
+            alignItems="center"
+          ></Box>
+          </>
   );
 };
+
+
--- server/src/user/user-roles.service.ts
+++ server/src/user/user-roles.service.ts
@@ -7,7 +7,6 @@ import { User } from 'src/database/models/users.model';
 import { Sequelize } from 'sequelize';
 
 
-
 @Injectable()
 export class UserRolesService {
   constructor(
@@ -38,6 +37,12 @@ export class UserRolesService {
     return null;
   }
 
+
+  async getAllRoles() {
+    return await this.rolesModel.findAll();
+  }
+  
+
   async getUserById(id: string): Promise<any | null> {
     try {
       const ParsedId = parseInt(id, 10);
@@ -45,17 +50,9 @@ export class UserRolesService {
         where: {
           id: ParsedId,
         },
-        attributes: [
-          'id',
-          'name',
-          'email',
-          'phone_number',
-          [Sequelize.col('users_roles.roles.value'), 'roles'],
-        ],
         include: [
           {
             model: UserRole,
-            attributes: [],
             include: [
               {
                 model: Role,
@@ -65,59 +62,160 @@ export class UserRolesService {
           },
         ],
       });
-      return user || null;
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  async updateUserById(id: string, data: any): Promise<any> {
-    try {
-      const ParsedId = parseInt(id, 10);
-
-      const user = await User.findOne({
-        where: {
-          id: ParsedId,
-        },
-      });
-
       if (!user) {
-        return null;
+        return null; 
       }
-
-      await user.update({
-        name: data.name,
-        phone_number: data.phone_number,
-      });
-
-      const roleValue = data.roles;
-      const updatedRole = await Role.findOne({
-        where: {
-          value: roleValue,
-        },
-      });
-
-      if (updatedRole) {
-        const associatedRole = await UserRole.findOne({
-          where: {
-            user_id: user.id,
-          },
-        });
-        console.log('associatedRole:', associatedRole);
-        if (associatedRole) {
-          await associatedRole.update({
-            role_id: updatedRole.id, 
-          });
-        }
-      }
-      return user;
+  
+      const data = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        phone_number: user.phone_number,
+        roles: user.users_roles.map((userRole) => userRole.roles.value),
+      };
+      return data;
     } catch (error) {
       throw error;
     }
   }
-  async getAllRoles() {
-    return await this.rolesModel.findAll();
+  
+
+
+  async updateUserById(id: string, data: any): Promise < any > {
+  try {
+    const ParsedId = parseInt(id, 10);
+
+    const user = await User.findOne({
+      where: {
+        id: ParsedId,
+      },
+      include: [
+        {
+          model: UserRole,
+          include: [Role],
+        },
+      ],
+    });
+
+    if(!user) {
+      return null;
+    }
+  
+      await user.update({
+      name: data.name,
+      phone_number: data.phone_number,
+    });
+
+    const updatedRoles = data.roles || [];
+    const currentRoles = user.users_roles.map((userRole) => userRole.roles.value);
+
+    const rolesToAdd = updatedRoles.filter((role) => !currentRoles.includes(role));
+    const rolesToRemove = currentRoles.filter((role) => !updatedRoles.includes(role));
+
+    for(const roleToRemove of rolesToRemove) {
+      const userRoleToRemove = user.users_roles.find((userRole) => userRole.roles.value === roleToRemove);
+      if (userRoleToRemove) {
+        await userRoleToRemove.destroy();
+      }
+    }
+
+      const rolesToAddPromises = rolesToAdd.map(async (roleToAdd) => {
+      const roleModel = await Role.findOne({ where: { value: roleToAdd } });
+      if (roleModel) {
+        await UserRole.create({
+          user_id: user.id,
+          role_id: roleModel.id,
+        });
+      }
+    });
+
+    await Promise.all(rolesToAddPromises);
+
+    return user;
+  } catch(error) {
+    throw error;
   }
 }
+  
+}
 
+
+
+  // async getUserById(id: string): Promise<any | null> {
+  //   try {
+  //     const ParsedId = parseInt(id, 10);
+  //     const user = await User.findOne({
+  //       where: {
+  //         id: ParsedId,
+  //       },
+  //       attributes: [
+  //         'id',
+  //         'name',
+  //         'email',
+  //         'phone_number',
+  //         [Sequelize.col('users_roles.roles.value'), 'roles'],
+  //       ],
+  //       include: [
+  //         {
+  //           model: UserRole,
+  //           attributes: [],
+  //           include: [
+  //             {
+  //               model: Role,
+  //               attributes: ['value'],
+  //             },
+  //           ],
+  //         },
+  //       ],
+  //     });
+  //     return user || null;
+  //   } catch (error) {
+  //     throw error;
+  //   }
+  // }
+
+  // async updateUserById(id: string, data: any): Promise<any> {
+  //   try {
+  //     const ParsedId = parseInt(id, 10);
+
+  //     const user = await User.findOne({
+  //       where: {
+  //         id: ParsedId,
+  //       },
+  //     });
+
+  //     if (!user) {
+  //       return null;
+  //     }
+
+  //     await user.update({
+  //       name: data.name,
+  //       phone_number: data.phone_number,
+  //     });
+
+  //     const roleValue = data.roles;
+  //     const updatedRole = await Role.findOne({
+  //       where: {
+  //         value: roleValue,
+  //       },
+  //     });
+
+  //     if (updatedRole) {
+  //       const associatedRole = await UserRole.findOne({
+  //         where: {
+  //           user_id: user.id,
+  //         },
+  //       });
+  //       console.log('associatedRole:', associatedRole);
+  //       if (associatedRole) {
+  //         await associatedRole.update({
+  //           role_id: updatedRole.id, 
+  //         });
+  //       }
+  //     }
+  //     return user;
+  //   } catch (error) {
+  //     throw error;
+  //   }
+  // }
 
