050a7f527dda9b11f3726ffb3e1c8ebcbedcdb5b
Ani Hambaryan <ani.hambaryan.ec@gmail.com>
2023-10-10 17:25:37 +0400

    App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,10 @@ import "./App.css";
 import { Admin, Resource } from "react-admin";
 import { MyLayout } from "./layouts/Layout";
 import HomeIcon from "@mui/icons-material/Home";
-import ContactsIcon from "@mui/icons-material/Contacts";
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import GrassIcon from "@mui/icons-material/Grass";
-import SpaIcon from "@mui/icons-material/Spa";
-import ConstructionIcon from "@mui/icons-material/Construction";
-import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
-import BarChartIcon from "@mui/icons-material/BarChart";
-import BuildIcon from "@mui/icons-material/Build";
-import TireRepairIcon from "@mui/icons-material/TireRepair";
 import VpnKeyIcon from "@mui/icons-material/VpnKey";
 import PermIdentityIcon from "@mui/icons-material/PermIdentity";
 import FaceRetouchingNaturalIcon from "@mui/icons-material/FaceRetouchingNatural";
+import ArticleIcon from "@mui/icons-material/Article";
 import polyglotI18nProvider from "ra-i18n-polyglot";
 import englishMessages from "ra-language-english";
 import armenianMessages from "ra-language-armenian";
@@ -23,12 +15,12 @@ import { MainDashboard } from "./pages/MainDashboard";
 import { Signin } from "./pages/auth/Signin";
 import { Signup } from "./pages/auth/Signup";
 import { Profile } from "./pages/auth/Profile";
-// import { AdminDesktop } from "./pages/admin/Desktop";
+import { AdminDesktop } from "./pages/admin/Desktop";
 import { ChooseDevice } from "./pages/admin/ChooseDevice";
-//import { ChooseCommunity } from "./pages/admin/ChooseCommunity";
+import { ChooseCommunity } from "./pages/admin/ChooseCommunity";
 import { FixedDeviceShow } from "./pages/admin/FixedDeviceShow";
-//import { UserShowAdm } from "./pages/admin/UserShow";
-//import { UserListAdm } from "./pages/admin/UserList";
+import { UserShowAdm } from "./pages/admin/UserShow";
+import { UserListAdm } from "./pages/admin/UserList";
 import { FixedDeviceListAdm } from "./pages/admin/FixedDeviceListAdm";
 import { PortableDeviceListAdm } from "./pages/admin/PortableDeviceListAdm";
 import { PortableDeviceShow } from "./pages/admin/PortableDeviceShow";
@@ -51,16 +43,11 @@ import { FieldEdit } from "./pages/owner/FieldEdit";
 //import { DeviceRequest } from "./pages/owner/DeviceRequest";
 import { GreenhouseShow } from "./pages/admin/GreenhouseShow";
 import { GreenhouseListAdm } from "./pages/admin/GreenhouseList";
-import { ProductList } from "./pages/owner/ProductList";
-import { ProductEdit } from "./pages/owner/ProductEdit";
-import { ProductCreate } from "./pages/owner/ProductCreate";
-import { PortableDeviceStatisticsPage } from "./pages/CommunityManager/PortableDeviceStatistics";
-import { FixedDeviceStatisticsPage } from "./pages/CommunityManager/FixedDeviceStatistics";
-import { FieldStatisticsPage } from "./pages/CommunityManager/FieldStatistics";
-import { DeviceStatisticPage } from "./pages/owner/DeviceStatistics";
-import { GreenhouseStatisticsPage } from "./pages/CommunityManager/GreenhouseStatistics";
+import { ProductListAdm } from "./pages/admin/ProductList";
+import { ProductShow } from "./pages/admin/ProductListAdm";
+// import { DeviceStatisticPage } from "./pages/admin/DeviceStatistic";
 import { Contact } from "./pages/auth/Contact";
-//import { CommunityManager } from "./pages/CommunityManager/Desktop";
+import { CommunityManager } from "./pages/CommunityManager/Desktop";
 import { UserList } from "./pages/CommunityManager/UserList";
 import { DesktopInfo } from "./pages/CommunityManager/DesktopInfo";
 import axios from "axios";
@@ -72,19 +59,13 @@ import customDataProvider from "./providers/dataProvider";
 //const dataProvider = jsonServerProvider(API_URL);
 /* const dataProvider = simpleRestProvider(API_URL); */
 import { authProvider } from "./providers/authPovider";
-// import { API_URL } from "./consts";
+import { API_URL } from "./consts";
 import myTheme from "./themes/general_theme";
 import { UserEdit } from "./pages/CommunityManager/UserEdit";
-
-// const customIconStyle = {
-//   color: '#00ff00', // Replace with your desired color code
-// };
-
 const i18nProvider = polyglotI18nProvider(
   (locale) => (locale === "am" ? armenianMessages : englishMessages),
   "en" // Default locale
 );
-const API_URL = process.env.REACT_APP_API_URL;
 const App = () => {
   const isAuthenticated = getJwtTokenFromCookies() ? true : false;
   const [roles, setRoles] = React.useState([]);
@@ -137,10 +118,11 @@ const App = () => {
     }
   }, []);
 
-  console.log("sf", process.env.REACT_APP_API_URL);
-  //const permission = perms;
+  // console.log(roles);
+  const permission = perms;
   const role = roles;
 
+  
   const commonResources = [
     <Resource
       name="dashboard"
@@ -154,16 +136,8 @@ const App = () => {
       icon={VpnKeyIcon}
       options={{ label: "Sign In" }}
     />,
-    <Route 
-      exact path="/signup" 
-      element={Signup} 
-    />,
-    <Resource 
-      name="signup" 
-      list={Signup} 
-      icon={ExitToAppIcon}
-      options={{ label: "Sign Up" }} 
-    />,
+    <Route exact path="/signup" element={Signup} />,
+    <Resource name="signup" list={Signup} options={{ label: "Sign Up" }} />,
   ];
 
   const employeeResources = [
@@ -182,7 +156,6 @@ const App = () => {
     <Resource
       name="contact"
       list={Contact}
-      icon={ContactsIcon}
       options={{ label: "My contacts" }}
     />,
   ];
@@ -204,7 +177,6 @@ const App = () => {
       name="contact"
       list={Contact}
       options={{ label: "My contacts" }}
-      icon={ContactsIcon}
     />,
     <Resource
       name="ownerPage"
@@ -212,64 +184,38 @@ const App = () => {
       icon={FaceRetouchingNaturalIcon}
       options={{ label: "Owner desktop" }}
     />,
-    // <Resource
-    //   name="greenhouses"
-    //   list={GreenhouseList}
-    //   create={GreenhouseCreate}
-    //   edit={GreenhouseEdit}
-    //   icon={Greenhouse}
-    //   options={{label: "Greenhouse"}}
-
-    // />,
-
     <Resource
       name="greenhouses"
       list={GreenhouseList}
       create={GreenhouseCreate}
       edit={GreenhouseEdit}
-      icon={GrassIcon} // Use Material-UI's LocalFlorist icon
-      options={{ label: "Greenhouse" }}
+      options={{label: "Greenhouse"}}
+      
     />,
-
     <Resource
       name="fields"
       list={FieldList}
       create={FieldCreate}
       edit={FieldEdit}
-      icon={SpaIcon}
-      options={{ label: "Field" }}
+      options={{label: "Field"}}
     />,
-    <Resource
-      name="devices"
-      list={DeviceDesktop}
-      icon={ConstructionIcon}
-      options={{ label: "Device" }}
-    />,
+    <Resource name="devices" list={DeviceDesktop} 
+    options={{label: "Device"}}/>,
     <Resource
       name="portable_devices"
       list={PortableDeviceList}
       create={PortableDeviceCreate}
       edit={PortableDeviceEdit}
-      icon={BuildIcon}
-      options={{ label: "Portable device" }}
+      options={{label: "Portable device"}}      
     />,
     <Resource
       name="fixed_devices"
       list={FixedDeviceList}
       create={FixedDeviceCreate}
       edit={FixedDeviceEdit}
-      icon={TireRepairIcon}
-      options={{ label: "Fixed device" }}
+      options={{label: "Fixed device"}}    
     />,
-    <Resource
-      name="products"
-      list={ProductList}
-      create={ProductCreate}
-      icon={ProductionQuantityLimitsIcon}
-      edit={ProductEdit}
-    />,
-    {
-      /* //<Resource
+    // <Resource
     //   name="fields"
     //   list={FieldList}
     //   create={FieldCreate}
@@ -279,8 +225,7 @@ const App = () => {
     //   name="device_requests_history"
     //   create={DeviceRequest}
     //   list={DeviceRequest}
-    // />, */
-    },
+    // />,
   ];
   const CMResources = [
     <Resource
@@ -293,7 +238,6 @@ const App = () => {
       name="contact"
       list={Contact}
       options={{ label: "My contacts" }}
-      icon={ContactsIcon}
     />,
     <Resource
       name="profile"
@@ -304,40 +248,14 @@ const App = () => {
     <Resource
       name="usersinfo"
       list={DesktopInfo}
-      icon={DashboardIcon}
       options={{ label: "Dashboard" }}
     />,
     <Resource
       name="community/users"
       list={UserList}
       edit={UserEdit}
-      // icon={ArticleIcon}
-      icon={PeopleAltIcon}
+      icon={ArticleIcon}
       options={{ label: "Users" }}
-    />,
-    <Resource
-      name="portable_device_statistics"
-      list={PortableDeviceStatisticsPage}
-      icon={BuildIcon}
-      options={{ label: "Portable Device Statistics" }}
-    />,
-    <Resource
-      name="fixed_device_statistics"
-      list={FixedDeviceStatisticsPage}
-      icon={TireRepairIcon}
-      options={{ label: "Fixed Device Statistics" }}
-    />,
-    <Resource
-      name="fields_statistics"
-      list={FieldStatisticsPage}
-      icon={SpaIcon}
-      options={{ label: "Field Statistics" }}
-    />,
-    <Resource
-      name="greenhouse_statistics"
-      list={GreenhouseStatisticsPage}
-      icon={GrassIcon}
-      options={{ label: "Greenhouse statistics" }}
     />,
   ];
   const AdminResources = [
@@ -346,52 +264,33 @@ const App = () => {
       name="all_fixedDevices"
       list={FixedDeviceListAdm}
       show={FixedDeviceShow}
-      icon={TireRepairIcon}
-      options={{ label: "Fixed device" }}
+      options={{label: "Fixed device"}}    
     />,
     <Resource
       name="all_portableDevices"
       list={PortableDeviceListAdm}
       show={PortableDeviceShow}
-      icon={BuildIcon}
-      options={{ label: "Portable device" }}
+      options={{label: "Portable device"}}    
     />,
     // <Resource name="User" list={UserListAdm} show={UserShowAdm} />,
-
+    
     // <Resource name="User" list={UserListAdm} show={UserShowAdm} />,
     <Resource
       name="Greenhouse"
       list={GreenhouseListAdm}
-      icon={GrassIcon}
       show={GreenhouseShow}
     />,
     //<Resource name="chooseCommunity" list={ChooseCommunity} />,
-    <Resource
-      name="chooseDevice"
-      list={ChooseDevice}
-      icon={ConstructionIcon}
-      options={{ label: "Choose device" }}
-    />,
+    <Resource name="chooseDevice" list={ChooseDevice} options={{label: "Choose device"}}    />,
     //<Resource name="User" list={UserListAdm} show={UserShowAdm} />,
     <Resource
       name="Greenhouse"
       list={GreenhouseListAdm}
-      icon={GrassIcon}
       show={GreenhouseShow}
     />,
-    <Resource
-      name="products"
-      list={ProductList}
-      create={ProductCreate}
-      icon={ProductionQuantityLimitsIcon}
-      edit={ProductEdit}
-    />,
 
-    <Resource
-      name="Statistic"
-      icon={BarChartIcon}
-      list={DeviceStatisticPage}
-    />,
+    <Resource name="Product" list={ProductListAdm} show={ProductShow} />,
+    // <Resource name="Statistic" list={DeviceStatisticPage} />,
     //<Resource name="community_manager" list={CommunityManager} />,
     // <Resource name="usersinfo" list={DesktopInfo} />,
     // <Resource
@@ -432,80 +331,67 @@ const App = () => {
         name="greenhouses"
         list={GreenhouseList}
         create={GreenhouseCreate}
-        icon={GrassIcon}
         edit={GreenhouseEdit}
       />,
       <Resource
         name="fields"
         list={FieldList}
         create={FieldCreate}
-        icon={SpaIcon}
         edit={FieldEdit}
       />,
       <Resource
         name="fixed_devices"
         list={FixedDeviceList}
         create={FixedDeviceCreate}
-        icon={TireRepairIcon}
         edit={FixedDeviceEdit}
       />,
       <Resource
         name="portable_devices"
         list={PortableDeviceList}
         create={PortableDeviceCreate}
-        icon={BuildIcon}
         edit={PortableDeviceEdit}
       />,
-      <Resource
-        name="products"
-        list={ProductList}
-        create={ProductCreate}
-        icon={ProductionQuantityLimitsIcon}
-        edit={ProductEdit}
-      />,
+      <Resource name="Product" list={ProductListAdm} show={ProductShow} />,
     ];
 
     const answer = [];
     let b = 0;
     const all_permissions = [
-      "EDIT_GREENHOUSE",
-      "EDIT_FIELD",
-      "EDIT_FIXED_DEVICE",
-      "EDIT_PORTABLE_DEVICE",
-      "EDIT_ROLE",
-      "EDIT_PRODUCT",
+      'EDIT_GREENHOUSE' ,
+      'EDIT_FIELD' ,
+      'EDIT_FIXED_DEVICE', 
+      'EDIT_PORTABLE_DEVICE' ,
+      'EDIT_ROLE' ,
+      'EDIT_PRODUCT' 
     ];
     for (let index = 0; index < perms.length; index++) {
       for (let j = 0; j < all_permissions.length; j++) {
-        if (perms[index] === all_permissions[j]) {
-          if (all_permissions[j] === "EDIT_ROLE") {
-            answer[b] = (
-              <Resource
-                name="usersinfo"
-                list={DesktopInfo}
-                icon={DashboardIcon}
-                options={{ label: "Dashboard" }}
-              />
-            );
-            b++;
-            answer[b] = (
-              <Resource
-                name="community/users"
-                list={UserList}
-                edit={UserEdit}
-                icon={PeopleAltIcon}
-                options={{ label: "Users" }}
-              />
-            );
-            b++;
-          } else {
-            answer[b] = permissions[j];
-            b++;
+        if (perms[index] == (all_permissions[j])) {
+          if (all_permissions[j]== "EDIT_ROLE") {
+              answer[b] = <Resource
+              name="usersinfo"
+              list={DesktopInfo}
+              options={{ label: "Dashboard" }}
+            />;
+              b++;
+              answer[b] = 
+                <Resource
+                  name="community/users"
+                  list={UserList}
+                  edit={UserEdit}
+                  icon={ArticleIcon}
+                  options={{ label: "Users" }}
+                />
+                b++;
+            }
+            else{
+              answer[b] = permissions[j];
+              b++;
+            }
           }
         }
       }
-    }
-
+    
     return answer.length > 0 ? answer : <div>...loading</div>;
   };
   return (
