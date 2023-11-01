6c46fe0cb9c15d83d7a96d8f802d4c4be5507e19
Lilith Asatrian <lilit.asatryan.ec@gmail.com>
2023-11-01 16:38:45 +0400

    swagger integration added in main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { config } from 'dotenv';
+// import { createSwagger } from '../swagger'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 config();
 async function start() {
@@ -15,6 +17,16 @@ async function start() {
     credentials: true,
     exposedHeaders: ['Content-Range'],
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Smart Farm')
+    .setDescription('Smart Farm API description')
+    .setVersion('1.0')
+    .addTag('smart_farm')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
---
+++ server/swagger.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from 'src/app.module';
+
+
+
+export const  createSwagger = async() => {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('Cats example')
+  .setDescription('The cats API description')
+  .setVersion('1.0')
+  .addTag('cats')
+  .build();
+
+//   const document = SwaggerModule.createDocument(app, config);
+//   SwaggerModule.setup('api', app, document);
+
+  await app.listen(7000);
+}
+
+createSwagger();
